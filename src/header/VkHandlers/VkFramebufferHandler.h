9c052bc11ded38d9a9ff60ebbff32d9a6b5ee4bf
Bastien Ruivo <bastien.ruivo@gmail.com>
2026-03-13 16:53:44 +0100

    Update VkFramebufferHandler.h

--- src/header/VkHandlers/VkFramebufferHandler.h
+++ src/header/VkHandlers/VkFramebufferHandler.h
@@ -8,6 +8,14 @@ class VkFramebufferHandler
     public:
     VkFramebufferHandler(VkDevice device, VkRenderPass renderPass, VkExtent2D imageExtent, std::vector<VkImageView> imageViews);
 
+    // VkHandles can be destroyed by one of the two object when copying, so we enable moving and disable copying.
+    // technically *for now* you can vk call directly to drestroy the VK thingy inside with the Get, honestly don't care for now, if the user want to explicitly do that it's his business, not mine.
+    VkFramebufferHandler(VkFramebufferHandler&& other);
+    VkFramebufferHandler& operator=(VkFramebufferHandler&& other);
+
+    VkFramebufferHandler(const VkFramebufferHandler&) = delete;
+    VkFramebufferHandler& operator=(const VkFramebufferHandler&) = delete;
+
     ~VkFramebufferHandler();
 
     VkFramebuffer Get() const;
