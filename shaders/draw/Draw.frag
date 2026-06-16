f13ef4fa466fb6699e628758b8eea91037944768
Bastien Ruivo <bastien.ruivo@gmail.com>
2026-06-16 12:44:27 +0200

    Broken state

--- shaders/draw/Draw.frag
+++ shaders/draw/Draw.frag
@@ -1,10 +1,23 @@
 #version 460
 
+// KEYWORD ALPHA_CUTOUT
+// KEYWORD DEBUG
+
 #include "Lighting.glsl"
 
 layout(early_fragment_tests) in;
 void main()
 {
     LightingInput lightingInput = CreateLightingInput(vMaterialIndex);
+
+    #if defined(ALPHA_CUTOUT)
+    {
+        if (lightingInput.Alpha < 0.5)
+        {
+            discard;
+        }
+    }
+    #endif
+
     outColor = vec4(ComputeLighting(lightingInput), 1.0);
 }
--- shaders/draw/Lighting.glsl
+++ shaders/draw/Lighting.glsl
@@ -3,6 +3,16 @@
 
 #include "../header/GlobalIncludes.glsl"
 
+layout(push_constant) uniform PushConstants
+{
+    BufferIndexes bufferIndex;
+#ifdef DEBUG
+    uint  debugIndex;
+#endif
+} push;
+
+#define DEBUG_NORMAL 1
+
 layout(set = GLOBAL_SET, binding = BINDING_TEXTURES) uniform sampler2D uTextures[];
 
 layout(set = GLOBAL_SET, binding = STATIC_BINDING_MATERIAL_BUFFER, std430) readonly buffer MaterialPropertiesBuffer
@@ -21,6 +31,7 @@ layout(location = 0) out vec4 outColor;
 struct LightingInput
 {
     vec3 BaseColor;
+    vec3 Normal;
 #ifdef ALPHA_CUTOUT
     float Alpha;
 #endif
@@ -30,9 +41,11 @@ LightingInput CreateLightingInput(uint materialIndex)
 {
     MaterialProperties material = _Materials.data[materialIndex];
     vec4 baseColor = texture(uTextures[material.BaseColorTexId], vTexcoords) * material.BaseColor;
+    vec3 normal = vNormal;
 
     LightingInput lightInput;
     lightInput.BaseColor = baseColor.rgb;
+    lightingInput.Normal = normal;
 #ifdef ALPHA_CUTOUT
     lightInput.Alpha = baseColor.a;
 #endif
@@ -41,6 +54,14 @@ LightingInput CreateLightingInput(uint materialIndex)
 
 vec3 ComputeLighting(LightingInput lightInput)
 {
+
+#ifdef DEBUG
+    if(debugIndex == DEBUG_NORMAL)
+    {
+        return vNormal;
+    }
+ #endif
+
     return lightInput.BaseColor;
 }
 
